36474c1a85b83a3ab51254896f8309d91085af19
Mot0511 <suvorov.matvej9@gmail.com>
2024-08-05 17:55:05 +0300

    _

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from fastapi import FastAPI
 import uvicorn
 from auth.router import auth_router
 from config import DB_URI
+from db.models import Base
 from db.db import create_db
 from posts.router import posts_router
 from messages.router import messages_router
 from starlette.middleware.cors import CORSMiddleware
+from db.db import engine
+
+Base.metadata.create_all(bind=engine)
 
 # Инициализация приложения
 app = FastAPI(
@@ -19,8 +23,7 @@ app.include_router(messages_router, tags=['Messages'])
 
 @app.get('/')
 def start():
-    return DB_URI
-    # return 'Server is working'
+    return 'Server is working'
 
 app.add_middleware(
     CORSMiddleware,
@@ -30,9 +33,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-async def start():
-    await create_db()
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# async def start():
+#     await create_db()
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
+# if __name__ == "__main__":
+#     asyncio.run(start())
+
 
 if __name__ == "__main__":
-    asyncio.run(start())
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
