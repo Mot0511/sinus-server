ae5da6988ff723974585f3fae91844011f9e3fa0
Mot0511 <suvorov.matvej9@gmail.com>
2024-08-06 10:02:03 +0300

    _

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ async def create_db_route():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=['http://localhost', 'https://sinus-client.vercel.app'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
